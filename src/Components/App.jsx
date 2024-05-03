b7e5048f6f0f1c829b8d4c4ae0196bed25bbf42e
Rasel <103592581+alfred-rasel@users.noreply.github.com>
2024-05-04 00:47:45 +0600

    navber and account section create

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,13 +1,13 @@
-
-
+import "../styles/App.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
 
 function App() {
- 
-
   return (
-    <div> hello world</div>
+    <Layout>
+      <Home />
+    </Layout>
   );
-   
 }
 
 export default App;
