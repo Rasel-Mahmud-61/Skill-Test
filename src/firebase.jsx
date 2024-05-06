777493406ce79bfbcc47d60b50c635a60b93c923
Rasel <103592581+alfred-rasel@users.noreply.github.com>
2024-05-06 12:31:43 +0600

    data fetch from database

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -8,6 +8,8 @@ const app = initializeApp({
   storageBucket:import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
   messagingSenderId:import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_REACT_APP_ID,
+  databaseURL:import.meta.env.VITE_REACT_APP_DATABASE_URL
+  
 });
 
 export default app;
